26b15fea1f20bf956ccc682e750b649d605aca07
Kim SeonHee <sh011226@naver.com>
2021-04-05 14:29:08 +0900

    first commit

---
+++ Project2/project12.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	char num1 = -10;
+	short num2 = 3000;
+	int num3 = -9876543210;
+	long num4 = 1937648250;
+	printf(" %d %d %d %1d \n", num1, num2, num3, num4);
+	return 0;
+}
